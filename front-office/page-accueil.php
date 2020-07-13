178b74544721e0d8d5cb6493c3ed34c30fecf187
Alexandra-frieh <67266090+Alexandra-frieh@users.noreply.github.com>
2020-07-13 16:24:04 +0200

    travail du début

---
+++ front-office/page-accueil.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>SILVER BLOG</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="style/front-office.css">
+</head>
+
+
+<h2>MON PROJET</h2>
+
+    <body>
+<p>Bonjour ! Je m'appelle Alex, et je suis créatrice de bijoux à mon temps perdu.
+<br>Mon projet est de vous partager les différents bijoux que j'ai crée. Ces bijoux sont bien évidemment fait main avec des matériaux de qualité.
+    <br>Je posterai régulièrement mes nouvelles créations, et vous aurez bien évidemment la possibilité de commenter et partager les publications.
+    <br>
+    <br>
+    <br>
+    <br>
+    Voici la liste ci-dessous des 5 derniers articles publiés.
+    <ul>
+        <li>Un pendentif Bicolore - Femme
+            <br> Image ?
+            <br> </li>
+            <li>Bague "Dana" Or blanc - Femme
+                <br> Image ?
+            <br> </li>
+            <li>Bague Acier et Diamant - Homme
+                <br> Image ?
+                <br> </li>
+                <li>Bracelet Argent - Femme
+                    <br>  Image ?
+                    <br>  </li>
+            <li>Chaine en or
+                <br> Image ?
+                <br> </li>
+            
+        </li>
+        
+    </ul>
+</p>
+
+</body>
+
+</html>
---
+++ front-office/page-articles.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>SILVER BLOG</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="style/front-office.css">
+<body>
+<ul>
+    <li>Un pendentif Bicolore - Femme
+        <br> Image ?
+        <br></li>
+        <li>Bague "Dana" Or blanc - Femme
+            <br> Image ?
+        <br>  </li>
+        <li>Bague Acier et Diamant - Homme
+            <br> Image ?
+            <br>  </li>
+            <li>Bracelet Argent - Femme
+                <br>  Image ?
+                <br> </li>
+        <li>Chaine en or
+            <br> Image ?
+            <br> </li>
+        
+    </li>
+    
+</ul>
+
+</body>
